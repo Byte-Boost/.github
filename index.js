140cdee41bce641a16973d60515a8ed3bd6c7ec3
Vinicius Forcato <76211125+nininhosam@users.noreply.github.com>
2023-06-02 15:53:56 -0300

    Finished dynamics, added basic current members;

--- index.js
+++ index.js
@@ -2,56 +2,54 @@
 // 2 - read json file, turn into object
 // 3 - profit
 
-const membersInfo = 
-[{
-  "user-id": "usuario1",
-  "name": "Nome do Nomedomeio Sobrenome",
-  "quote": "Frase de impacto, inspiracional, ou que defina o membro",
-  "socials": {
-    "github": "githublink",
-    "linkedin": "linkedinlink",
-    "facebook": "facebooklink",
-    "instagram": "instagramlink",
-    "gmail": "gmaillink",
-    "other": "otherlink"
-  }
-},
-{
-  "user-id": "usuario2",
-  "name": "Outronome do Outronomedomeio Outrosobrenome",
-  "quote": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis a eligendi natus non, qui hic dicta earum pariatur cupiditate, illum iure error voluptatum deserunt nemo esse! Iure incidunt pariatur modi laborum inventore iusto libero dicta accusamus ipsa, esse voluptatibus doloribus suscipit dolore quisquam voluptatem dolorum!",
-  "socials": {
-    "github": "githublink",
-    "linkedin": "linkedinlink",
-    "facebook": "facebooklink",
-    "instagram": "instagramlink",
-    "gmail": "gmaillink"
-  }
-},
-{
-  "user-id": "usuario3",
-  "name": "---- -----",
-  "quote": " ",
-  "socials": {
-    "github": "githublink",
-    "linkedin": "linkedinlink",
-    "discord": "discordlink",
-    "gmail": "gmaillink",
-    "other": "otherlink"
-  }
-}]
-let allTeamArea = document.querySelector("#AllTeamMembers");
-console.log(membersInfo[0])
-
-let createTag = (elementType, parentEl, className, idName="")=>{
+const loadInfo = async (cb) => {
+  fetch('./membersInfo.json')
+    .then((response) => response.json())
+    .then((json) => {
+      cb(json);
+    });
+};
+let allTeamArea = document.querySelector('#AllTeamMembers');
+let createTag = (elementType, parentEl, className, idName) => {
   let result = document.createElement(`${elementType}`);
-  result.setAttribute("class", `${className}`);
-  result.setAttribute("id", `${idName}`);
+  result.setAttribute('class', `${className}`);
+  result.setAttribute('id', `${idName}`);
   parentEl.appendChild(result);
   return result;
-}
-membersInfo.forEach(member=>{
-  let memberArea = createTag("div", allTeamArea, "member-area")
-  let memberIndex = membersInfo.indexOf(member)
-  if (memberIndex % 2 != 0) memberArea.setAttribute("data-member-parity", "even")
-})
+};
+loadInfo((membersInfo) => {
+  membersInfo.forEach((member) => {
+    let memberArea = createTag('div', allTeamArea, 'member-area', 'memberArea');
+    let memberIndex = membersInfo.indexOf(member);
+    if (memberIndex % 2 != 0) memberArea.setAttribute('data-member-parity', 'even');
+
+    let memberStatic = createTag('div', memberArea, 'member-static', 'memberStatic');
+    let memberPfp = createTag('img', memberStatic, 'member-pfp', 'memberPfp');
+    fetch(`./assets/${member.userId}.jpg`)
+      .then((img) => {
+        if (img.ok) {
+          memberPfp.setAttribute('src', `./assets/${member.userId}.jpg`);
+        } else {
+          memberPfp.setAttribute('src', `./assets/blank.jpg`);
+        }
+      })
+      .catch((err) => console.log('error: ', err));
+    let memberName = createTag('h2', memberStatic, 'member-name', 'memberName');
+    memberName.innerText = member.name;
+
+    let memberDynamic = createTag('div', memberArea, 'member-dynamic', 'memberDynamic');
+    let memberQuote = createTag('p', memberDynamic, 'member-quote', 'memberQuote');
+    memberQuote.innerText = member.quote;
+    let memberSocials = createTag('div', memberDynamic, 'member-socials', 'memberSocial');
+    if (memberIndex % 2 != 0) memberSocials.setAttribute('data-member-parity', 'even');
+    socialKeys = Object.keys(member.socials);
+    socialKeys.forEach((key) => {
+      let socialIcon = createTag('a', memberSocials, 'social-icon', 'socialIcon');
+      socialIcon.setAttribute('href', `${member.socials[key]}`);
+      socialIcon.setAttribute('target', '_blank');
+      let socialIconImg = createTag('img', socialIcon, 'social-icon-img', 'socialIconImg');
+      socialIconImg.setAttribute('src', `./assets/${key}-logo.png`);
+      socialIconImg.setAttribute('alt', `${key}`);
+    });
+  });
+});
